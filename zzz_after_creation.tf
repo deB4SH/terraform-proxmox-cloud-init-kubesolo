834b7145d25c6fb967856f3c275bda603271a562
deB4SH <github@b4sh.de>
2025-05-29 17:02:03 +0200

    rm depends on

--- zzz_after_creation.tf
+++ zzz_after_creation.tf
@@ -17,7 +17,6 @@ module "kube-config" {
 }
 
 resource "local_file" "kube-config" {
-  depends_on = [ kube-config.stdout ]  
   for_each = {for each in var.vm: each.name => each}
 
   content         = module.kube-config[each.key].stdout
